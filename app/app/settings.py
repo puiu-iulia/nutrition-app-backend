47e382f9dfc66a251e6d5db77043c6fa0649ffa9
Iulia Puiu <iulia.puiu@hotmail.com>
2022-03-14 10:49:17 +0200

    Allow expo as host for testing

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zu=f_7k1mpq6o)wcmvwb1tev1@kwooueflsve1itg+^)jr0n82
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
